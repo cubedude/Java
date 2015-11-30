6dd58a232c39f93dd4d17ed9daefe863529a631c
cubedude <taavi.metsvahi@gmail.com>
2015-12-01 01:38:12 +0200

    Kommenteerimine

--- src/smartSignature/SmartSignature.java
+++ src/smartSignature/SmartSignature.java
@@ -11,6 +11,10 @@ import javafx.stage.Stage;
 import smartSignature.DataClass;
 import smartSignature.OutlookHandler;
 
+/**
+ * Created by Taavi Metsvahi
+ */
+
 public class SmartSignature extends Application{
 	//Create objects to work with
 	DataClass connection = new DataClass();
@@ -24,37 +28,53 @@ public class SmartSignature extends Application{
 	private Scene loginScene;
 	private Scene loggedScene;
 
+	//Include images to use
     Image logo = new Image("http://smartconnections.ee/resources/img/sig_title.png");
     Image icon = new Image("http://smartconnections.ee/resources/img/sig_iconpng");
     ImageView logoView = new ImageView(logo);
     
-	
+
 	public static void main(String[] args) throws Exception {
     	launch(args);
 	}
 
+    /**
+     * Starts application
+     * @param Stage primaryStage
+     */
 	@Override
     public void start(Stage primaryStage) {
     	//Create window to work with
     	window = primaryStage; 	
+    	//Set window title
     	window.setTitle(guiTitle);
+    	//Set icon of the programm
     	window.getIcons().add(icon);
+    	//Restrict user input
     	window.setResizable(false);
     	//Build and attach login screen
     	buildLogin();
     }
 
+    /**
+     * Builds login window
+     */
     private void buildLogin() {   
     	//New grid
         GridPane grid = new GridPane();
+        //Set padding for edges
         grid.setPadding(new Insets(guiPadding, guiPadding, guiPadding, guiPadding));
+        //Set gui width
         grid.setPrefWidth(guiWidth);
+        //Set vertical distance
         grid.setVgap(5); 
+        //Attach stylesheet
         grid.getStylesheets().add("/smartSignature/signature.css");
 
-    	//Logo 
+    	//Insert logo to the top
         GridPane.setConstraints(logoView, 0, 0);
         
+        //New name abel, add padding and set its place
         Label nameLabel = new Label("Kasutajanimi:");
         nameLabel.getStyleClass().add("top_padding_20");
         GridPane.setConstraints(nameLabel, 0, 2);
@@ -76,63 +96,89 @@ public class SmartSignature extends Application{
         loginButton.setPrefWidth(guiWidth);
         GridPane.setConstraints(loginButton, 0, 6);
 		
+        //Add everything to grid and apply
+        grid.getChildren().addAll(logoView, nameLabel, nameInput, passLabel, passInput, loginButton);
+        loginScene = new Scene(grid);
+
+        //Set the scene to the window 
+        window.setScene(loginScene);
+        //Show the application
+        window.show();        
+        
+        /*
+         *  ACTIONS
+         */
+        
         //Login action
         loginButton.setOnAction(e -> {
+            //If name input length is less than 3
         	if(nameInput.getText().length() < 3){
-        		System.out.println("name input error");
+                //Give error by adding ErrorInput class to it
         		nameInput.getStyleClass().add("errorInput");
         		passInput.getStyleClass().remove("errorInput");
         	}
+            //If pass input length is less than 3
         	else if(passInput.getText().length() < 3){
-        		System.out.println("password input error");
+                //Give error by adding ErrorInput class to it
         		nameInput.getStyleClass().remove("errorInput");
         		passInput.getStyleClass().add("errorInput");
         	}
         	else{
+                //If all correct, remove any error classes
         		nameInput.getStyleClass().remove("errorInput");
         		passInput.getStyleClass().remove("errorInput");
-        		
+
+                //Set name into connection object
         		connection.setName(nameInput.getText());
+                //Set password into connection object
 				connection.setPassword(passInput.getText());
+                //Clear the option value just in case
+				connection.setOption("");
+
+                //Connect with connection object to the web
 				int status = connection.connect();
-				
+
+                //If the status is 0, it was a success
         		if(status == 0){
-        			if(false) BuildLoggedIn();
-            		System.out.println("success");
+                    //Build a logged in veiw
+        			BuildLoggedIn();
         		}else{
-            		System.out.println("connection error");
+                    //If the status is not 0, it was a failure, display errors in inputs
+            		System.out.println("Error: "+connection.getError());
             		nameInput.getStyleClass().add("errorInput");
             		passInput.getStyleClass().add("errorInput");
         		}
         	} 
         });
-        
-        //Add everything to grid and apply
-        grid.getChildren().addAll(logoView, nameLabel, nameInput, passLabel, passInput, loginButton);
-        loginScene = new Scene(grid);
-        
-        window.setScene(loginScene);
-        window.show();        
     }
 
+    /**
+     * Builds the logged in screen
+     */
     private void BuildLoggedIn() {   
     	//New grid
         GridPane grid = new GridPane();
+        //Set padding for edges
         grid.setPadding(new Insets(guiPadding, guiPadding, guiPadding, guiPadding));
+        //Set gui width
         grid.setPrefWidth(guiWidth);
+        //Set vertical distance
         grid.setVgap(5); 
-        
-    	//Logo ofc
+        //Attach stylesheet
+        grid.getStylesheets().add("/smartSignature/signature.css");
+
+    	//Insert logo to the top
         GridPane.setConstraints(logoView, 0, 0);
     	
 
         //Add everything to grid and apply
         grid.getChildren().addAll(logoView);
-        grid.getStylesheets().add("smartSignature/signature.css");
         loggedScene = new Scene(grid);
-        
+
+        //Set the scene to the window 
         window.setScene(loggedScene);
-        window.show();
+        //Show the application
+        window.show();        
     }
 
         
